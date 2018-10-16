e7ef8d73b1f79a950a92e6408c53309afa7304f3
SOF <animeshk55@gmail.com>
2018-10-16 10:04:35 +0530

    few modification in header navbar

--- src/App.js
+++ src/App.js
@@ -19,32 +19,34 @@ class App extends Component {
       <Navbar inverse collapseOnSelect>
   <Navbar.Header>
     <Navbar.Brand>
-      <a href="#brand">React-Bootstrap</a>
+      <a href="#iView Labs">iView</a>
     </Navbar.Brand>
     <Navbar.Toggle />
   </Navbar.Header>
   <Navbar.Collapse>
     <Nav>
       <NavItem eventKey={1} href="#">
-        Link
+        Smart Tracking
       </NavItem>
       <NavItem eventKey={2} href="#">
-        Link
+        Smart Travel
       </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-      </NavDropdown>
+      <NavItem eventKey={3} href="#">
+        Loyalty Aggregator
+      </NavItem>
+      <NavItem eventKey={4} href="#">
+        Smart HealthCare
+      </NavItem>
     </Nav>
     <Nav pullRight>
       <NavItem eventKey={1} href="#">
-        Link Right
+        Login
       </NavItem>
       <NavItem eventKey={2} href="#">
-        Link Right
+        Google Login
+      </NavItem>
+      <NavItem eventKey={3} href="#">
+        Facebook Login
       </NavItem>
     </Nav>
   </Navbar.Collapse>
