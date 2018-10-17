3005eba20349f80213fdb269674a8468e972dda5
SOF <animeshk55@gmail.com>
2018-10-17 10:19:49 +0530

    Deleted few files as oauth was giving error

--- src/index.js
+++ src/index.js
@@ -3,39 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import express from 'express'
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
 
-const app = express()
-
-passport.use(
-    new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken) => {
-        console.log(accessToken);
-    } )
-    );
-
-    app.get(
-        '/auth/google',
-        passport.authenticate('google', {
-            scope: ['profile', 'email']
-        })
-        );
-    
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-
-// client id : 4885617406-o9pnu4lus5cll6vbrocpacu0ige26d7a.apps.googleusercontent.com
-
-// client secret : hOY9ldVDYWXcT3Bdg1Jz2pwr
